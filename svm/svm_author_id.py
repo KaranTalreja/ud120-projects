d20a7096d14f8b9372751b523994168262b60410
Karan Talreja <karantalreja91@gmail.com>
2014-12-07 00:45:50 +0530

    [Problem & Solution]: SVM classifier for email data set
    1. C parameter increases accuracy as it increases.
       Tuned to 10000 with accuracy 99% for full data set
    2. SVM very slow with respect to GaussianNB. (more than 200sec compared to 30sec)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(kernel = "rbf",cache_size=2000,C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t0, 3), "s"
+acc = accuracy_score(labels_test, pred)
+print "Accuracy: ",acc
+for i in [10,26,50]:
+    print "Test Set Element",i,"Label: ",pred[i]
+print "Number of Chris's Emails", sum(pred)
 #########################################################
 
 
