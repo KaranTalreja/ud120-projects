26f8dc2c05fed89983b6ed71f06cc5454b2ae4e7
Karan Talreja <karantalreja91@gmail.com>
2014-12-10 01:11:21 +0530

    [Exploring Enron Data Set]: Printed the required data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,13 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+print "Number of People in the Data Set:",len(enron_data)
+print "Number of features of first person:",(enron_data.keys())[0],":",len(enron_data[(enron_data.keys())[0]])
+countOfPoi = 0
+for key,values in dict(enron_data).iteritems():
+    countOfPoi += values["poi"]
+print "Featues available ",values.keys()
+print "Number of POI in the data:",countOfPoi
+print "Total stock value of James Prentice:",enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "Number of emails from wesley colwell to POI:",enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
+print "Value of stock options exercised by Jeffrey Skilling:", enron_data["SKILLING JEFFREY K"]['exercised_stock_options']
