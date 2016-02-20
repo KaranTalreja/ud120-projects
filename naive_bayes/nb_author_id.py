25c8aa8c624128ccda503463b4d7c1b5e32c0b8a
Karan Talreja <karantalreja91@gmail.com>
2016-02-20 22:46:39 +0530

    Added code for 3 classifiers
    Gaussian Naive Bayes
    Support Vector Machines
    Decision Trees

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,21 +13,26 @@
 import sys
 from time import time
 sys.path.append("../tools/")
+sys.path.append("../choose_your_own/")
 from email_preprocess import preprocess
-
+from class_vis import prettyPicture
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+# print features_train.shape, features_test.shape, labels_train.shape, labels_test.shape
 
 
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn.naive_bayes import GaussianNB 
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, pred)
 #########################################################
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC()
 
+clf.fit(features_train[:len(features_train)/1],labels_train[:len(labels_train)/1])
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_test, pred)
 #########################################################
 
 
