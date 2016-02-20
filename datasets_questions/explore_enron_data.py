06f5b5922b28ae80a550d9326f59b201916a82a8
Karan Talreja <karantalreja91@gmail.com>
2016-02-21 01:09:47 +0530

    Added code for all quizzes from Lesson-5
    Getting to know enron data better

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,20 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+print "Number of people",len(enron_data)
+for key,item in enron_data.iteritems():
+	print "Number of features per person", len(item.keys()), item.keys()
+	break
+print "Number of POI",sum([1 for key,item in enron_data.iteritems() if item["poi"] == 1])
+print "Total value of the stock belonging to James Prentice", enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "Email messages from Wesley Colwell to persons of interest", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print "value of stock options exercised by Jeffrey Skilling",  enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+CFO = "FASTOW ANDREW S"
+CEO = "SKILLING JEFFREY K"
+CHAIRMAN = "LAY KENNETH L"
+for key in [CFO, CEO, CHAIRMAN]:
+	print key, enron_data[key]["total_payments"] 
+print "Number of quantifiable salaries",sum([1 for key,item in enron_data.iteritems() if item["salary"] != "NaN"])
+print "Number of known email ids",sum([1 for key,item in enron_data.iteritems() if item["email_address"] != "NaN"])
+print "Number of known people with unknown total payments",sum([1 for key,item in enron_data.iteritems() if item["total_payments"] == "NaN"])
+print "Number of known POI with unknown total payments",sum([1 for key,item in enron_data.iteritems() if item["total_payments"] == "NaN" and item["poi"] == 1])
